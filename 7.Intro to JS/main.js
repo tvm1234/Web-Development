21aef4be7517f33198ea380a8bbbddf8735d4bee
Tanmay Mokashi <tanmay0204mokashi@gmail.com>
2025-02-17 18:29:45 +0530

    js

---
+++ 7.Intro to JS/main.js
@@ -0,0 +1,16 @@
+// console.log("Hi");
+//  var hi = "Tanmay";
+//  console.log(hi);
+
+//  var a =5;
+//  console.log(5 + a);
+
+ function add(num1, num2){
+    console.log(num1 + num2)
+ }
+add(5,7)
+ 
+function beverage(drink , quantity){
+    console.log(`I want`+" "+ quantity + " "+ drink)
+}
+beverage(`cola` ,5)
