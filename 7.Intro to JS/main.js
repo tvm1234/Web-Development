27db6b197733dfd152dbeb600a73398ed50b577f
Tanmay Mokashi <tanmay0204mokashi@gmail.com>
2025-02-18 11:33:18 +0530

    js practice

--- 7.Intro to JS/main.js
+++ 7.Intro to JS/main.js
@@ -1,16 +1,38 @@
-// console.log("Hi");
-//  var hi = "Tanmay";
-//  console.log(hi);
+// // // console.log("Hi");
+// // //  var hi = "Tanmay";
+// // //  console.log(hi);
 
-//  var a =5;
-//  console.log(5 + a);
+// // //  var a =5;
+// // //  console.log(5 + a);
 
- function add(num1, num2){
-    console.log(num1 + num2)
- }
-add(5,7)
+// // //  function add(num1, num2){
+// // //     console.log(num1 + num2)
+// // //  }
+// // // add(5,7)
  
-function beverage(drink , quantity){
-    console.log(`I want`+" "+ quantity + " "+ drink)
+// // // function beverage(drink , quantity){
+// // //     console.log(`I want`+" "+ quantity + " "+ drink)
+// // // }
+// // // beverage(`cola` ,5)
+
+
+// // var x =5;
+// // var y = 6;
+// // function add (num1 , num2){
+// //     console.log(x + num1)
+// // }
+// // add (6, 7);
+
+// // console.log(y)
+
+// function findMaxValue(arr) {
+//     // Write your code here
+//     // return the result
+  
+//     return Math.max(...arr);
+//   }
+
+function sum(a,b){
+    console.log(a*a + b)
 }
-beverage(`cola` ,5)
+sum (5,6)
