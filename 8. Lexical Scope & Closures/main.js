9dd310f7d62dad233bb6d4d42760265b02749673
Tanmay Mokashi <tanmay0204mokashi@gmail.com>
2025-02-19 17:15:06 +0530

    practice

--- 8. Lexical Scope & Closures/main.js
+++ 8. Lexical Scope & Closures/main.js
@@ -1,6 +1,13 @@
-console.log(a);
-var a = 10;
-function test() {
-    console.log('This is a test function');
-}
-test();
+// console.log(a);
+// var a = 10;
+// function test() {
+//     console.log('This is a test function');
+// }
+// test();
+
+const pi = 3.14;
+let person = "John Doe";
+let answer = 'Yes I am!';
+console.log(pi);
+console.log(person);
+console.log(answer);
