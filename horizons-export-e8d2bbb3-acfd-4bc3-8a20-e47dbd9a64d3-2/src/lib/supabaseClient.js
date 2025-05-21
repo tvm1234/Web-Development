b5a96656be8f294b1c10db7a255da7e9659d7d2b
Tanmay Mokashi <94227213+tvm1234@users.noreply.github.com>
2025-05-21 08:51:27 +0530

    Delete horizons-export-e8d2bbb3-acfd-4bc3-8a20-e47dbd9a64d3-2 directory

--- horizons-export-e8d2bbb3-acfd-4bc3-8a20-e47dbd9a64d3-2/src/lib/supabaseClient.js
+++
@@ -1,7 +0,0 @@
-
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://vairjyzbertlgzkrufhi.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZhaXJqeXpiZXJ0bGd6a3J1ZmhpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDczNjMzNDUsImV4cCI6MjA2MjkzOTM0NX0.XUaDa1nxs2xy7RMrr0oB9hU-3PMjrtmHy_eHuQeOvnM';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
